b01dd1a21e40acd3bc8047d156a21e566d1cc5c4
Nick <nhoszko@gmail.com>
2019-11-14 21:55:09 +0000

    parameterised the queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,21 +1,24 @@
 const { Pool } = require('pg');
 const process = require('process');
 
+
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 });
-
+console.log(process.argv);
 ////////////////////////////////////////////////////
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 2};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`,[`%${cohortName}%`, limit] )
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`)
@@ -23,27 +26,31 @@ LIMIT ${process.argv[3] || 2};
 })
 .catch(err => console.log('query error', err.stack));
 ///////////////////////////////////////////////////
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5
-`)
-.then(res => {
-  // console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack))
-////////////////////////////////////////////////////
-pool.query(`
-SELECT students.id, students.name, cohorts.name AS cohortName
-FROM students
-  JOIN cohorts ON students.cohort_id = cohorts.id
-LIMIT 2;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    // console.log(user);
-    // console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohortname} cohort`);
-  })
-})
-.catch(err => console.error('query error', err.stack));
+// pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5
+// `)
+// .then(res => {
+//   // console.log(res.rows);
+// })
+// .catch(err => console.error('query error', err.stack))
+// ////////////////////////////////////////////////////
+// pool.query(`
+// SELECT students.id, students.name, cohorts.name AS cohortName
+// FROM students
+//   JOIN cohorts ON students.cohort_id = cohorts.id
+// LIMIT 2;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     // console.log(user);
+//     // console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohortname} cohort`);
+//   })
+// })
+// .catch(err => console.error('query error', err.stack));
 
+
+
+// '%${process.argv[2]}%'
+// ${process.argv[3] || 2};
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,6 +8,8 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+console.log(process.argv);
 pool.query(`
 SELECT teachers.name AS teacher,
        cohorts.name AS cohort,
@@ -18,14 +20,18 @@ FROM assistance_requests
     JOIN students ON students.id = student_id
     JOIN cohorts ON cohorts.id = cohort_id
 
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name
 
-`)
+`, [`%${cohortName}%`] )
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`)
   })
 })
 .catch(err => console.log('query error', err.stack));
+
+
+
+// '${process.argv[2] || 'JUL02'}'
